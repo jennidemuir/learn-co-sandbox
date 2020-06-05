562d2d0d674bb656800bf340791efed62dfef200
Jennifer Paquette <jennidemuir@gmail.com>
2020-06-05 16:14:38 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,10 @@
+#run_code_inside = false
+#puts "Code before if...end"
+#if run_code_inside
+ # puts "code inside"
+#end
+#puts "Code after if...end"
+
+
+this_year = 2019
+puts "Hey, it's not 2019!" unless this_year == 2019
